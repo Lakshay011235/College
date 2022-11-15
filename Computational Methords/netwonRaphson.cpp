f6bb8e05e41346a7990e0e22795c7f956a84fcfd
Lakshay011235 <lakshay.alt@gmail.com>
2022-11-15 22:54:26 +0530

    newtonraphson in cm

---
+++ Computational Methords/netwonRaphson.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+double f(double x) {return x*x*x + x - 1;}
+double df(double x){return 3*x*x + 1;}
+
+int main()
+{   
+    double x; int iter=100;
+    cout << "Equation: x*x*x + x - 1" << endl;
+    cout << "Assumed root:";
+    cin >> x;
+    cout << "Iteration\tx\t\tf(x)\n";
+    for(int i = 0;i<iter;i++){
+        x = x - (f(x)/df(x));
+        cout << to_string(i+1)+"\t\t"+to_string(x)+"\t"+to_string(f(x))+"\n";
+        if (abs(f(x)) < 0.00000001) break;
+    }
+    
+    cout << "\nThe nearest root is:" << x << "\n";
+    cout << "\nWritten by: Lakshay Sharma 02396402721" << endl;
+    return 0;
+}
