20a941a92b728e43d369224c4eb12d9b549b597e
Lakshay011235 <lakshay.alt@gmail.com>
2022-11-15 22:57:56 +0530

    lagranges in cm

---
+++ Computational Methords/lagrangesPolynomial.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<float> split(string str) {
+    string str1;
+    vector<float> vect;
+    string search;
+    for (int i=0; i < str.length(); i++ ) {
+        search = str[i];
+        if (search != "\t") {
+            str1 = str1 + str[i];
+        }
+        else {
+            vect.push_back(stof(str1));
+            str1 = "";
+        }
+    }
+    vect.push_back(stof(str1));
+    return  vect;
+}
+
+int main()
+{
+    string abc;
+    vector<float> vect1,vect2;
+    int val;
+    cout << "Use tab to seperate\n";
+    cout << "X:\t";
+    getline(cin,abc);
+    vect1 = split(abc);
+    cout << "F(x):\t";
+    getline(cin,abc);
+    vect2 = split(abc);
+    cout << "value of X:";
+    cin >> val;
+    float prod = 1, sum = 0;
+    for (int i = 0; i < vect1.size(); i++) {
+            for (int j = 0; j < vect1.size(); j++) {
+                if (i != j) {
+                    prod = prod*((val - vect1[j])/(vect1[i] - vect1[j]));
+                }
+            }
+            prod = prod*vect2[i];
+            sum = sum + prod;
+            prod = 1;
+    }
+    cout << "Ans:" << sum;
+    cout << "\nWritten by: Lakshay Sharma 02396402721" << endl;
+    return 0;
+}
