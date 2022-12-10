8453aec857178784e397a1ed5eb05ff9c8ff7069
Lakshay011235 <lakshay.alt@gmail.com>
2022-12-10 14:08:03 +0530

    unit1,2 oops done

---
+++ OOPs/question12.cpp
@@ -0,0 +1,6 @@
+/*
+Create a base class basic info with data members name, roll no, sex and two member functions getdata 
+and display. Derive a class physical fit from basic_info which has data members height and weight and 
+member functions getdata display. Display all the information using object of derived class.(Single 
+Inheritance)
+*/
