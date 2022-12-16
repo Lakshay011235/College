9ab983561846fc083324039371dd6507f2960eb9
Lakshay011235 <lakshay.alt@gmail.com>
2022-12-16 20:06:23 +0530

    .

---
+++ DS/Experiment 1/q4_MatrixMultiplication.cpp
@@ -0,0 +1,14 @@
+/*
+4. Write a program showing the implementation of Matrix Multiplication.
+*/
+#include <iostream>
+
+int main()
+{
+    int m1=10, n1=10, m2=10, n2=10;
+    int mat1[m1][n1];
+    int mat2[m2][n2];
+    matrixInput(mat1, m1, n1);
+
+    return 0;
+}
