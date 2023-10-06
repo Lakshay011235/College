cc5f303a25adb8f0ac3e4b8fb126244fc5de3507
Lakshay Sharma <107244796+Lakshay011235@users.noreply.github.com>
2023-10-06 14:47:45 +0530

    some noteworthy changes

--- OS/OS_Exp3.c
+++ OS/OS_Exp3.c
@@ -1,14 +1,16 @@
-#include<stdio.h>
+#include <stdio.h>
 
 // Structure to represent a process
-struct Process {
-    int id;             // Process ID
-    int priority;       // Priority of the process (higher value means higher priority)
-    int burstTime;      // Burst time or execution time of the process
-    int arrivalTime;    // Arrival time of the process
+struct Process
+{
+    int id;          // Process ID
+    int priority;    // Priority of the process (higher value means higher priority)
+    int burstTime;   // Burst time or execution time of the process
+    int arrivalTime; // Arrival time of the process
 };
 
-int main() {
+int main()
+{
     // Number of processes
     int n;
 
@@ -18,7 +20,8 @@ int main() {
     struct Process processes[n];
 
     // Input process details
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         processes[i].id = i + 1;
         printf("Enter priority for process %d: ", i + 1);
         scanf("%d", &processes[i].priority);
@@ -29,10 +32,13 @@ int main() {
     }
 
     // Sort processes based on priority (and arrival time in case of tie)
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - i - 1; j++) {
-            if (processes[j].priority < processes[j + 1].priority || 
-                (processes[j].priority == processes[j + 1].priority && processes[j].arrivalTime > processes[j + 1].arrivalTime)) {
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (processes[j].priority < processes[j + 1].priority ||
+                (processes[j].priority == processes[j + 1].priority && processes[j].arrivalTime > processes[j + 1].arrivalTime))
+            {
                 // Swap processes if they are out of order
                 struct Process temp = processes[j];
                 processes[j] = processes[j + 1];
@@ -40,6 +46,22 @@ int main() {
             }
         }
     }
+    // TODO: NEEEEEDDD TO ADD QUEUE OF THIS !!!!!!!!
+    //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    
+    // Pre-emptive sorting
+    for (int i = 0; i < n - 1; i++)
+    {
+        int remainingTime = processes[i].burstTime - (processes[i + 1].arrivalTime - processes[i].arrivalTime);
+        if (processes[i + 1].burstTime < remainingTime)
+        {
+            struct Process temp = processes[i];
+            processes[i] = processes[i + 1];
+            processes[i + 1] = temp;
+        }
+    }
+
     int waitingTime[n], turnaroundTime[n];
 
     waitingTime[0] = 0;
@@ -57,7 +79,7 @@ int main() {
     for (int i = 0; i < n; i++)
     {
         printf("%d\t\t%d\t\t%d\t\t", processes[i].id, processes[i].priority, processes[i].arrivalTime);
-        printf("%d\t\t%d\t\t%d\n",  processes[i].burstTime, waitingTime[i], turnaroundTime[i]);
+        printf("%d\t\t%d\t\t%d\n", processes[i].burstTime, waitingTime[i], turnaroundTime[i]);
     }
     printf("\n");
 
